d0a695aedc796245c02eb790b468c810ac981e08
daniel-fors <daniel.lennart.fors@gmail.com>
2024-10-05 17:03:38 +0200

    added boids

---
+++ Assets/Scripts/Boids.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Boid
+{
+    public Vector3 Position;
+    public Vector3 Velocity;
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Boid)) return false;
+
+        Boid boid = (Boid)obj;
+
+        return boid.Position.Equals(Position) && boid.Velocity.Equals(Velocity);
+    }
+}
+
+public class Boids : MonoBehaviour
+{
+    public Vector3 Space = Vector3.one * 50;
+    public int NumBoids = 100;
+    public float VisualRange = 5f;
+    public float MaxVelocity = 2f;
+    public float CenteringFactor = 0.02f;
+    public float AvoidDistance = 3.0f;
+    public float AvoidFactor = 5f;
+    public float MatchingFactor = 0.2f;
+    public float BoundsMargin = 0.3f;
+    public float TurnFactor = 0.08f;
+
+    private List<Boid> boids = new();
+    private List<GameObject> physicalBoids = new();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(Space * 0.5f, Space);
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < NumBoids; i++)
+        {
+            boids.Add(
+                new()
+                {
+                    Position = new(Random.value * Space.x, Random.value * Space.y, Random.value * Space.z),
+                    Velocity = new(Random.value * MaxVelocity - MaxVelocity * 0.5f, Random.value * MaxVelocity - MaxVelocity * 0.5f, Random.value * MaxVelocity - MaxVelocity * 0.5f),
+                });
+
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = boids[i].Position;
+            physicalBoids.Add(sphere);
+        }
+    }
+
+    private void FlyTowardsCenter(ref Boid boid)
+    {
+        Vector3 centroid = Vector3.zero;
+        int numBoids = 0;
+        
+        // Including self
+        foreach (Boid b in boids)
+        {
+            if (Vector3.Distance(boid.Position, b.Position) < VisualRange)
+            {
+                centroid += b.Position;
+                numBoids++;
+            }
+        }
+
+        centroid /= numBoids;
+        boid.Velocity += (centroid - boid.Position) * CenteringFactor * Time.deltaTime;
+    }
+    private void AvoidOthers(ref Boid boid)
+    {
+        Vector3 move = Vector3.zero;
+
+        foreach (Boid b in boids)
+        {
+            if (!boid.Equals(b) && Vector3.Distance(boid.Position, b.Position) < AvoidDistance)
+            {
+                move += (boid.Position - b.Position);
+            }
+        }
+
+        boid.Velocity += move * AvoidFactor * Time.deltaTime;
+    }
+    private void MatchVelocity(ref Boid boid)
+    {
+        Vector3 velocity = Vector3.zero;
+        int numBoids = 0;
+
+        // Including self
+        foreach (Boid b in boids)
+        {
+            if (Vector3.Distance(boid.Position, b.Position) < VisualRange)
+            {
+                velocity += b.Velocity;
+                numBoids++;
+            }
+        }
+
+        velocity /= numBoids;
+        boid.Velocity += (velocity - boid.Velocity) * MatchingFactor * Time.deltaTime;
+    }
+    private void LimitSpeed(ref Boid boid)
+    {
+        if (boid.Velocity.magnitude > MaxVelocity)
+        {
+            boid.Velocity = boid.Velocity.normalized * MaxVelocity;
+        }
+    }
+    private void KeepWithinBounds(ref Boid boid)
+    {
+        Vector3 Margin = Space * BoundsMargin;
+
+        if (boid.Position.x < Margin.x)
+        {
+            boid.Velocity.x += TurnFactor * Time.deltaTime;
+        }
+        if (boid.Position.x > Space.x - Margin.x)
+        {
+            boid.Velocity.x -= TurnFactor * Time.deltaTime;
+        }
+        if (boid.Position.y < Margin.y)
+        {
+            boid.Velocity.y += TurnFactor * Time.deltaTime;
+        }
+        if (boid.Position.y > Space.y - Margin.y)
+        {
+            boid.Velocity.y -= TurnFactor * Time.deltaTime;
+        }
+        if (boid.Position.z < Margin.z)
+        {
+            boid.Velocity.z += TurnFactor * Time.deltaTime;
+        }
+        if (boid.Position.z > Space.z - Margin.z)
+        {
+            boid.Velocity.z -= TurnFactor * Time.deltaTime;
+        }
+
+        if (boid.Position.x < 0.0f && boid.Velocity.x < 0.0f || boid.Position.x > Space.x && boid.Velocity.x > 0.0f)
+        {
+            boid.Velocity.x = -boid.Velocity.x;
+        }
+        if (boid.Position.y < 0.0f && boid.Velocity.y < 0.0f || boid.Position.y > Space.y && boid.Velocity.y > 0.0f)
+        {
+            boid.Velocity.y = -boid.Velocity.y;
+        }
+        if (boid.Position.z < 0.0f && boid.Velocity.z < 0.0f || boid.Position.z > Space.z && boid.Velocity.z > 0.0f)
+        {
+            boid.Velocity.z = -boid.Velocity.z;
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < boids.Count; i++)
+        {
+            Boid boid = boids[i];
+
+            FlyTowardsCenter(ref boid);
+            AvoidOthers(ref boid);
+            MatchVelocity(ref boid);
+            LimitSpeed(ref boid);
+            KeepWithinBounds(ref boid);
+
+            boid.Position += boid.Velocity * Time.deltaTime;
+            physicalBoids[i].transform.position = boid.Position;
+            boids[i] = boid;
+        }
+    }
+}
