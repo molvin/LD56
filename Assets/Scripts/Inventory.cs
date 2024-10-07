15fec60c392762aa36dca1eca9072cfcb44eaabc
molvin <akerstrom.perry@gmail.com>
2024-10-07 18:10:31 +0200

    asd

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -13,7 +13,7 @@ public class Inventory : MonoBehaviour
 
     private void Start()
     {
-        AddNewWeapon(Weapons.GravityPull);
+        AddNewWeapon(Weapons.Default);
     }
 
     public Weapon PeekNextWeapon()
