3e493420fedece406e3cba1a53c8789c2d1836b1
molvin <akerstrom.perry@gmail.com>
2024-10-07 21:03:39 +0200

    asd

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -7,4 +7,13 @@ public class Shop : MonoBehaviour
     public float Radius;
     public Transform CameraPos;
     public Transform PlayerPos;
+    public Follower[] Followers;
+
+    public void Init(Weapon[] weapons)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            Followers[i].Init(weapons[i], null, false);
+        }
+    }
 }
